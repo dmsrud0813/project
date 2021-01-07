9f8cde62dd851345deaadf3bb5a351ca97b660bc
dmsrud0813 <67427492+dmsrud0813@users.noreply.github.com>
2021-01-07 20:33:55 +0900

    보드게임 DB 만드는 중
    print(game_chart) 를 하면 ul 태그 안의 li가 안불러와집니다. @-@

--- boarGamdDB.py
+++ boarGamdDB.py
@@ -7,21 +7,18 @@ db = client.dbsparta
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-base_url = 'https://boardlife.co.kr/game_rank.php?tb=&pg='
-end_url = '&file=&file2=&view_id='
-page_num = 1
+url = 'https://www.koreaboardgames.com/boardgame/game_list01.php#pagenum=&view_type=bg&search_sort='
 
-new_url = base_url + str(page_num) + end_url
-data = requests.get(new_url, headers=headers)
+#gameList
+data = requests.get(url, headers=headers)
 soup = BeautifulSoup(data.text,'html.parser')
 
-game_chart = soup.select('body > table')
-#tbody > tr > td > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(1)
-print(game_chart)
-print(game_chart)
-link_tag=[]
-for game in game_chart:
-        link_tag = game.select_one('tbody > tr > td > table > tbody > tr > td > table > tbody > tr > td > table#new_game6501 > tbody > tr > td.game_row > table > tbody > tr > td.ellip > a.game_title')
-        if link_tag is not None :
-            print(link_tag)
+game_chart = soup.select('#gameList')
 
+print(game_chart)
+
+# for game in game_chart:
+#         link_tag = game.select_one(
+#         if link_tag is not None :
+#             print(link_tag)
+#
