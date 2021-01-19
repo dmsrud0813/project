9f170448894b2439ed05c76fc301144150a278d7
dmsrud0813 <67427492+dmsrud0813@users.noreply.github.com>
2021-01-19 20:20:15 +0900

    2021.01.19
    1. 모든 카테고리가 undefined로 출력 (DB에는 정상적으로 담겨있음)
    2. 좋아요 기능 추가
     - 하트 버튼 클릭 시 하트가 빨갛게 변했으면 좋겠는데 안됨...

---
+++ app.py
@@ -0,0 +1,37 @@
+from pymongo import MongoClient
+
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+
+# HTML 화면 보여주기
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/list', methods=['GET'])
+def show_games():
+    games = list(db.boardGameDB.find({},{'_id':False}))
+
+    return jsonify({'result': 'success', 'game_list': games})
+
+@app.route('/api/like', methods=['POST'])
+def like_game():
+    title = request.form['title']
+    game_title = db.boardGameDB.find_one({'title': title})
+    if game_title['like']== 0 :
+        plus_like = game_title['like']+1
+        db.boardGameDB.update_one({'title': title}, {'$set': {'like': plus_like}})
+        return jsonify({'result': 'success', 'msg': '좋아요 목록에 추가되었습니다.'})
+    else :
+        minus_like = game_title['like']-1
+        db.boardGameDB.update_one({'title': title}, {'$set': {'like': minus_like}})
+        return jsonify({'result': 'success', 'msg': '좋아요 목록에서 삭제되었습니다.'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
--- boarGamdDB.py
+++ boarGamdDB.py
@@ -6,19 +6,48 @@ client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-url = 'https://www.koreaboardgames.com/boardgame/game_list01.php#pagenum=&view_type=bg&search_sort='
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+url = 'https://www.koreaboardgames.com/_proc/get_products_list.php'
 
-#gameList
-data = requests.get(url, headers=headers)
-soup = BeautifulSoup(data.text,'html.parser')
+page_num = 1
 
-game_chart = soup.select('#gameList')
+for i in range(62):
+    data = {'pagenum': page_num, 'view_type': 'bg'}
+    chart = requests.post(url, data).json()['rows']
+    chart_num = 0
+    for i in range(10):
+        game_title = chart[chart_num]['prd_name']
+        gmae_player = chart[chart_num]['game_pers']
+        game_playtime = chart[chart_num]['game_time']
+        game_thumbnail = chart[chart_num]['img'].replace('<','')
+        game_thubnail2= game_thumbnail.replace('height="230"','')
+        game_thumbnail3 = game_thubnail2.replace('>','')
+        game_thumbnail4 = game_thumbnail3.replace('img src=','')
+        thumbnail = game_thumbnail4.replace('"','')
+        game_id=chart[chart_num]['idx']
+        game_url = 'https://www.koreaboardgames.com/boardgame/game_view.php?prd_idx='+ game_id
+        detail_data = requests.get(
+            game_url,
+           headers=headers)
+        soup = BeautifulSoup(detail_data.text, 'html.parser')
+        game_category = soup.select_one('#container > div.gameDetail > div.gameInfo > div.innerbox > div.infoBox > ul:nth-child(2) > li:nth-child(6)')
+        if game_category is not None :
+            category = game_category.text
+        else :
+            category = 'none'
+        doc = {
+            'title': game_title,
+            'player': gmae_player,
+            'playtime': game_playtime,
+            'img_url': thumbnail,
+            'catetory': category,
+            'like': 0
+        }
+        db.boardGameDB.insert_one(doc)
+        print('완료!', game_title)
+        chart_num = chart_num + 1;
+    page_num = page_num+1
 
-print(game_chart)
 
-# for game in game_chart:
-#         link_tag = game.select_one(
-#         if link_tag is not None :
-#             print(link_tag)
-#
+
+
